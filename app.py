88816e27a5672953d797752ebf6eea8056b9ac14
alamin <alaminprogramerr@gmail.com>
2025-01-30 21:09:08 +0600

    Test 2

--- app.py
+++ app.py
@@ -194,7 +194,7 @@ def submit_registration():
         msg = Message(
             "New Registration Submission", 
             sender=os.getenv('MAIL_USERNAME'), 
-            recipients=["softdev.alamin@gmail.com", "alaminprogramerr@gmail.com"]
+            recipients=["info@americanapd.com", "aramouni@americanapd.com"]
         )
         msg.body = "A new customer has submitted their registration form. The details are attached."
 
